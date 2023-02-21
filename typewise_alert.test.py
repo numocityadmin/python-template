c063c3b11a1de2cf28b6244e9458cefdd7a5e21d
Sparsha Bindu B S <sparshabsbindu@gmail.com>
2023-02-21 11:47:15 +0530

    state the problem

---
+++ typewise_alert.test.py
@@ -0,0 +1,11 @@
+import unittest
+import typewise_alert
+
+
+class TypewiseTest(unittest.TestCase):
+  def test_infers_breach_as_per_limits(self):
+    self.assertTrue(typewise_alert.infer_breach(20, 50, 100) == 'TOO_LOW')
+
+
+if __name__ == '__main__':
+  unittest.main()

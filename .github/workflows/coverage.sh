a7798c3ea45f7e1f30fe9ad3a4748e5fcc85066b
Sparsha Bindu B S <sparshabsbindu@gmail.com>
2023-03-25 13:05:39 +0530

    Added lint.sh and coverage.sh files

---
+++ .github/workflows/coverage.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+coverage run -m unittest discover
+coverage html
+coverage report
+COVERAGE=$(coverage report | awk 'END{print $NF}' | sed 's/[^0-9]*//g')
+if [ "$COVERAGE" -lt 100 ]; then
+    echo "Coverage is less than 100%. Failing the build..."
+    exit 1
+else
+    echo "Coverage is greater than or equal to 100%."
+fi

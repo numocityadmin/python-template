d87ed4218d6c843793c7163225837b70e5c7f9d2
sparsha-bindu <85055472+sparsha-bindu@users.noreply.github.com>
2023-03-27 11:21:32 +0530

    Update lint.sh

--- .github/workflows/lint.sh
+++ .github/workflows/lint.sh
@@ -3,4 +3,4 @@
 # stop the build if there are Python syntax errors or undefined names
 flake8 . --count --select=E9,F63,F7,F82 --show-source --statistics
 # exit-zero treats all errors as warnings.
-flake8 . --count --exit-zero --max-complexity=3 --max-line-length=80 --ignore=E111,E121,E126 --statistics
+flake8 . --count --max-complexity=3 --max-line-length=80 --ignore=E111,E121,E126 --statistics
